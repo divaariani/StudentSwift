5e598050cf158916263c7a26cb19634baf301a66
divaariani <divaariani@apps.ipb.ac.id>
2024-11-06 09:54:31 +0700

    add login view

---
+++ StudentSwift/LoginModel.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class LoginModel {
+    var username: String
+    var password: String
+
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+}
